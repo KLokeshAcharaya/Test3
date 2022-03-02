18b7688d169ab9e08f599e2a9548192be6f302d2
KLokeshAcharaya <lokesh.acharya@ics-global.in>
2022-03-02 12:59:10 +0530

    Add project

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using SingletonDemo.SingletonDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeachaer = Singleton.GetInstance;
+            fromTeachaer.PrintDetails("From Teacher");
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
+        }
+    }
+}
